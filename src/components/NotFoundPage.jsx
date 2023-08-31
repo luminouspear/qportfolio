003338c6dc4ad2ed70404c98364d8d5e10f70876
luminouspear <luminouspears@gmail.com>
2023-08-30 23:20:06 -0400

    404 update.

--- src/components/NotFoundPage.jsx
+++ src/components/NotFoundPage.jsx
@@ -1,9 +1,16 @@
+import { useEffect } from "react";
 import { styles } from "../styles";
 
 const NotFoundPage = () => {
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, []);
+
 	return (
 		<div className="container w-full h-screen mx-16 mt-[20%] max-w-7xl">
-			<h1 className={`${styles.sectionHeadText} text-6xl pb-4 text-primary-green font-bold pt-[15%]`}>
+			<h1
+				className={`${styles.sectionHeadText} text-6xl pb-4 text-primary-green font-bold pt-[15%]`}
+			>
 				404.
 			</h1>
 			<h2 className={styles.darkSectionSubText}>
